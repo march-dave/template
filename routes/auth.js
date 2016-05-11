c48641162d2fea05a1001672d20ecccb634d53d3
march-dave <ysangy@gmail.com>
2016-05-11 08:10:09 -0700

    add read api to router

--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,54 @@
 var express = require('express');
 var router = express.Router();
 
-router.post('/github', (req, res) => {
+var request = require('request');
+var Base64 = require('js-base64').Base64;
+
+var API_KEY = process.env.PB_API_KEY || '0hMBssXxtibQGOV7GcLTLAYAK3PmyJEd';
+var SECRET = process.env.PB_SECRET || 'oMejtq6OkTViROhN';
+
+router.get('/getpitneybowes', (req, res) => {
   // var accessTokenUrl = 'https://github.com/login/oauth/access_token';
   // var userApiUrl = 'https://api.github.com/user';
 
+  console.log('getpitneybowes');
 
-  var oauthUrl = 'https://api.pitneybowes.com/oauth/token';
-  var key = Base64.encode(`${API_KEY}:${SECRET}`);
+    var oauthUrl = 'https://api.pitneybowes.com/oauth/token';
+    var key = Base64.encode(`${API_KEY}:${SECRET}`);
+    // if(err) return res.status(400).send(err);
 
+
+
+    request.post({
+      url: oauthUrl,
+      headers: {
+        'Authorization': `Basic ${key}`,
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: "grant_type=client_credentials"
+    }, (err, response, body) => {
+      var accessToken = JSON.parse(body).access_token
+
+      var apiUrl = 'https://api.pitneybowes.com/location-intelligence/geoenhance/v1/address/bylocation?latitude=37.4954193&longitude=-121.9178031999999&searchRadius=2640&searchRadiusUnit=feet'
+
+      console.log('accessToken:', accessToken)
+
+      request.get({
+        url: apiUrl,
+        headers: {
+          'Authorization': `Bearer ${accessToken}`
+        }
+      }, (err, response, body) => {
+        console.log('body:', JSON.stringify(JSON.parse(body), null, 2));
+      });
+    });
+
+
+
+
+
+
+    res.send();
 
 });
 
